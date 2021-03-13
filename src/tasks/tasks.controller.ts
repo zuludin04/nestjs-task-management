d3575820df6c3133c7ff01326d961d50136f1a0e
zuludin <zmauludin04@gmail.com>
2021-03-13 16:25:59 +0700

    create mode interface

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
+// import { v1 as uuid} from 'uuid';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private taskService: TasksService) {}
 
     @Get()
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.taskService.getAllTasks();
     }
 }
