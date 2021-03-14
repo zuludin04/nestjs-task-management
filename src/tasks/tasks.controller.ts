764cb6b36e269cf1b94428b0ad2456aa3a2d8560
zuludin <zmauludin04@gmail.com>
2021-03-14 12:16:19 +0700

    delete task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private taskService: TasksService) {}
+    constructor(private taskService: TasksService) { }
 
     @Get()
     getAllTasks(): Task[] {
@@ -13,7 +13,7 @@ export class TasksController {
     }
 
     @Get('/:id')
-    getTaskById(@Param('id')id: string): Task {
+    getTaskById(@Param('id') id: string): Task {
         return this.taskService.getTaskById(id);
     }
 
@@ -22,5 +22,10 @@ export class TasksController {
         @Body() createTaskDto: CreateTaskDto
     ): Task {
         return this.taskService.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    deleteTaskById(@Param('id') id: string): void {
+        this.taskService.deleteTaskById(id);
     }
 }
