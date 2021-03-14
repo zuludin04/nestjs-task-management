5fd5d765066fd0f3b0a000e118458812b7c6e1dd
zuludin <zmauludin04@gmail.com>
2021-03-14 16:18:05 +0700

    custom validation pipe to check invalid status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -23,7 +24,7 @@ export class TasksController {
     }
 
     @Patch('/:id/status')
-    updateTaskStatus(@Param('id') id: string, @Body('status') taskStatus: TaskStatus): Task {
+    updateTaskStatus(@Param('id') id: string, @Body('status', TaskStatusValidationPipe) taskStatus: TaskStatus): Task {
         return this.taskService.updateTaskStatus(id, taskStatus);
     }
 
